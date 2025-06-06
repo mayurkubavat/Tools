076a90cd67acae8ca701d4bc6b6c35d4c3660762
mayurkubavat <mkubavat01@gmail.com>
2025-06-06 02:33:41 -0700

    Cleanup

--- sim.py
+++ sim.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python3
+"""A script to compile, simulate, and clean SystemVerilog projects using Xilinx tools."""
 
 from os import system
-
 import argparse
 
-#Get command line options
-parser = argparse.ArgumentParser(description='')
+DEFAULT_TOP_MODULE ='top'
 
-#Get options from command
-parser.add_argument('--clean', action='store_true', help='Clean LOGs..')
-parser.add_argument('--top', type=str, default='testbench', help='Specify testbench name (default: testbench)')
+parser = argparse.ArgumentParser( # type: ignore
+  description='Compiles and simulates SystemVerilog designs, or cleans generated files.'
+) # type: ignore
+
+parser.add_argument( # type: ignore
+  '--clean',
+  action='store_true',
+  help='Clean simulation-generated files and directories.'
+) # type: ignore
+parser.add_argument( # type: ignore
+  '--top',
+  type=str,
+  default=DEFAULT_TOP_MODULE,
+  help=f'Specify top-level module name. Assumes <top>.f file list. (default: {DEFAULT_TOP_MODULE})'
+)
 
 args = parser.parse_args()
 
 
 def build():
-        system(f'xvlog -sv -f testbench.f')
+  """Compiles the SystemVerilog source files listed in <top>.f."""
+
+  system(f'xvlog -work work -sv -f {args.top}.f')
+
 
 def sim():
-        system(f'xelab work.{args.top} -R')
+  """Elaborates the design and runs the simulation for the specified top module."""
+
+  system(f'xelab work.{args.top} -R')
 
 
 def clean():
-    system('rm -rf xsim.dir *.log *.pb *.jou *.log')
+  """Removes simulation-generated files and directories."""
+
+  print("Cleaning simulation files...")
+  system('rm -rf xsim.dir *.log *.pb *.jou')
 
 
 def main():
-    if args.clean:
-        clean()
-    else:
-        build()
-        sim()
+  """Main execution flow: either cleans or builds and simulates."""
+
+  if args.clean:
+    clean()
+  else:
+    build()
+    sim()
 
 
 if __name__ == '__main__':
-    main()
+  main()
