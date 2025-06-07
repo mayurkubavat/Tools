22684a6d029a2c62684d60bd87437151449dae96
Mayur Kubavat <mkubavat01@gmail.com>
2025-06-06 20:43:20 -0700

    Compile Flow Update

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 source /tools/Xilinx/2025.1/Vivado/settings64.sh
 
-export PATH=$PATH:$DV/git/Tools
-export PROJECT=pwd
+# Add DV/git/Tools to PATH if not already present
+case ":$PATH:" in
+  *":$DV/git/Tools:"*) ;; # Already in PATH, do nothing
+  *) export PATH="$PATH:$DV/git/Tools" ;; # Not in PATH, add it
+esac
--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A script to compile, simulate, and clean SystemVerilog projects using Xilinx tools."""
-
+import os
 from os import system
 import argparse
 
@@ -19,16 +19,34 @@ parser.add_argument( # type: ignore
   '--top',
   type=str,
   default=DEFAULT_TOP_MODULE,
-  help=f'Specify top-level module name. Assumes <top>.f file list. (default: {DEFAULT_TOP_MODULE})'
+  help=f'Specify top-level module name. Used for elaboration and for the default file list if --filelist or --sv is not provided. (default: {DEFAULT_TOP_MODULE})'
+) # type: ignore
+parser.add_argument( # type: ignore
+  '--filelist',
+  type=str,
+  help='Specify the Verilog file list (.f file). Overrides --sv and the default <top>.f.'
+)
+parser.add_argument( # type: ignore
+  '--sv',
+  type=str,
+  help="Specify SystemVerilog source files or pattern (e.g., '*.sv', 'src/*.sv'). Used if --filelist is not provided. Overrides the default <top>.f."
 )
 
 args = parser.parse_args()
 
+# Determine the source specification for compilation
+compile_source = ""
+if args.filelist:
+    compile_source = f"-f {args.filelist}"
+elif args.sv:
+    compile_source = args.sv
+else:
+  # If neither --filelist nor --sv is provided and not cleaning, it's an error.
+  parser.error("Either --filelist or --sv must be provided.")
 
 def build():
-  """Compiles the SystemVerilog source files listed in <top>.f."""
-
-  system(f'xvlog -work work -sv -f {args.top}.f')
+  """Compiles the specified SystemVerilog source files."""
+  system(f'xvlog -work work -sv {compile_source}')
 
 
 def sim():
